b6bfb67a4d9173d717ee9682b7ba9f27084425d3
Mahadev Shindhe <mahadev.shindhe@gmail.com>
2024-01-12 13:00:58 +0530

    Initial commit

---
+++ .storybook/preview.js
@@ -0,0 +1,20 @@
+import '../src/index.css';
+import { initialize, mswLoader } from 'msw-storybook-addon';
+
+initialize();
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  loaders: [mswLoader],
+};
+
+export default preview;
